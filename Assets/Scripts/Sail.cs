c4565a4256d32b6d6f791cbae56b2b43997915e6
Hüseyin <kircihuseyin524@gmail.com>
2026-05-13 19:08:35 +0300

    :/

--- Assets/Scripts/Sail.cs
+++ Assets/Scripts/Sail.cs
@@ -118,7 +118,7 @@ public class Sail : NetworkBehaviour
 
         float angleOfAttack = Vector3.Angle(currentSailForward, wind.normalized);
 
-
+        //TODO itiş gücü daha açıya daha merhametli olacak ve rüzgara dik olmadığı sürece bir miktar itiş sağlıyacak
         float lift = Mathf.Sin(angleOfAttack * Mathf.Deg2Rad * 2) * liftCoefficient;
         float drag = Mathf.Max(0, Vector3.Dot(currentSailForward, wind.normalized)) * 0.5f;
 
