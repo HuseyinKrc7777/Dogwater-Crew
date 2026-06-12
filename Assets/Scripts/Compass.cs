c8c3896ff928b572a9eb4168c76b1b7bf050cd48
Hüseyin <kircihuseyin524@gmail.com>
2026-06-13 01:40:45 +0300

    compass

---
+++ Assets/Scripts/Compass.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+public class Compass : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform stick;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        Vector3 forw = Vector3.forward;
+        if(transform.parent!=null)
+        {
+            forw = transform.parent.InverseTransformDirection(forw);
+        }
+        stick.localRotation = Quaternion.LookRotation(forw);
+    }
+
+}
