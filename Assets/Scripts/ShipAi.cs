2314ebb0d228031f5d0adc58b613d51b3e599ca3
Hüseyin <kircihuseyin524@gmail.com>
2026-07-16 15:27:44 +0300

    gemi verilen kordinatlara doğru gidiyor , çok oyunculu testi yapılmadı

--- Assets/Scripts/ShipAi.cs
+++ Assets/Scripts/ShipAi.cs
@@ -1,5 +1,6 @@
 using Unity.Netcode;
 using Unity.Services.Matchmaker.Models;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
@@ -7,6 +8,11 @@ public class ShipAi : NetworkBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] Ship ship;
+    [SerializeField] public LatitudeCoordinate target_latitude = new();
+    [SerializeField] public LongitudeCoordinate target_longitude = new();
+    [SerializeField] private Vector3 targetPos;
+    [SerializeField] private GlobalCoordinate coordinate;
+    [SerializeField] float targetDist;
     void Start()
     {
 
@@ -17,6 +23,9 @@ public class ShipAi : NetworkBehaviour
         base.OnNetworkSpawn();
         if(!IsServer)
             return;
+        float targetDist = coordinate.CalculateDistanceBetweenTwoPoints(coordinate.latitude.Value,coordinate.longitude.Value,target_latitude,target_longitude);
+        Vector3 targetDir = coordinate.CalculateDirectionBetweenTwoPoints(coordinate.latitude.Value,coordinate.longitude.Value,target_latitude,target_longitude);
+        targetPos = targetDir * targetDist + transform.position;
     }
     // Update is called once per frame
     void Update()
@@ -27,23 +36,49 @@ public class ShipAi : NetworkBehaviour
         {
             Vector3 forw = WaterController.Instance.wind.Value.normalized;
 
+            forw = ship.transform.InverseTransformDirection(forw);
+
+            /*
+            Vector3 forw = Wind.normalized;
             if(transform.parent!=null)
             {
                 forw = transform.parent.InverseTransformDirection(forw);
-
                 if(forw == Vector3.zero)
                 {
-                    forw = Vector3.forward;
+                    forw = Vector3.down;
                 }
             }
-
+            Flag.transform.localRotation = Quaternion.LookRotation(forw);
+            */
 
         
             foreach(Sail sail in ship.sailList)
-            {
-                forw.y = 0;
+            {           
                 sail.transform.localRotation = Quaternion.LookRotation(forw);
             }
+            targetDist = coordinate.CalculateDistanceBetweenTwoPoints(coordinate.latitude.Value,coordinate.longitude.Value,target_latitude,target_longitude);
+
+            if(targetDist < ship.GetComponent<Rigidbody>().linearVelocity.magnitude * 15)
+            {
+                foreach(Sail sail in ship.sailList)
+                {
+                    if(sail.openRope.currentValue.Value > 0.1f)
+                        sail.openRope.currentValue.Value -=0.1f;
+                }
+                
+            }
+            else
+            {
+                foreach(Sail sail in ship.sailList)
+                {
+                    if(sail.openRope.currentValue.Value < 0.9f)
+                        sail.openRope.currentValue.Value +=0.1f;
+                }
+            }
+            Vector3 targetDir = coordinate.CalculateDirectionBetweenTwoPoints(coordinate.latitude.Value,coordinate.longitude.Value,target_latitude,target_longitude);
+            Quaternion targetRot = Quaternion.LookRotation(targetDir);
+            transform.localRotation = Quaternion.Lerp(transform.localRotation,targetRot,0.1f);
+
         }
     }
 }
