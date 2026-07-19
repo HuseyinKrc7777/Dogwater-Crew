0f0eefc4718c8755baef95a6b12c15e44311aee3
Hüseyin <kircihuseyin524@gmail.com>
2026-07-19 20:47:57 +0300

    ada kontrolü network list e geçti

--- Assets/Scripts/WorldIslandController.cs
+++ Assets/Scripts/WorldIslandController.cs
@@ -5,6 +5,7 @@ using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.Rendering.Universal.Internal;
 using System.Threading.Tasks;
+using Unity.Collections;
 [Serializable]
 public class WorldIsland
 {
@@ -15,14 +16,34 @@ public class WorldIsland
     public int loadDistance = 900;
 }
 [Serializable]
+public struct IslandSpawnData : INetworkSerializable ,IEquatable<IslandSpawnData>
+{
+    public int index;
+    public Vector3 position;
+    public Quaternion rotation;
+    public int loadDistance;
+
+    public bool Equals(IslandSpawnData other)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void NetworkSerialize<T>(BufferSerializer<T> serializer)
+        where T : IReaderWriter
+    {
+        serializer.SerializeValue(ref index);
+        serializer.SerializeValue(ref position);
+        serializer.SerializeValue(ref rotation);
+    }
+
+}
+[Serializable]
 public class WorldIslandController : NetworkBehaviour
 {
     //TODO burasının tamamı baştan yazılacak
     public List<WorldIsland> worldIslands = new();
-    
-    List<WorldIsland> loadedIslands = new();
-    List<WorldIsland> loadQueue = new();
-    List<WorldIsland> unLoadQueue = new();
+    //rpc lerden liste dinyecisine geçilmesi denendi fakat başarısız olundu , daha sonra tekrar denenebilir.    
+    NetworkList<IslandSpawnData> loadedIslands = new();
 
     float counter = 0.0f;
     float timeout = 1.0f;
@@ -44,11 +65,10 @@ public class WorldIslandController : NetworkBehaviour
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
-        //böyle bırakıldığında oyuna ilk girildiğinde yükleme mesafesinde bir
-        // ada varsa herkeste birazcık farklı konumda olacaktır.
-        // bunun olmaması için oyunda yüklü olan adaların indexleri ve pozisyon / rotasyonları tutulmalı liste halinde network variable olarak tutulmalı
-        // sonra burada spawn edilmeli.
-        CheckIslandsToSpawnOrDespawnThem();
+        foreach(IslandSpawnData data in loadedIslands)
+        {
+            _ = SpawnIsland(data);
+        }
     }
 
     void FixedUpdate()
@@ -67,68 +87,26 @@ public class WorldIslandController : NetworkBehaviour
     {
         if (shipCoordinate == null)
             return;
-        List<WorldIsland> temp = loadedIslands.ToList<WorldIsland>();
         // TODO gelecekte buraya her adanın her zaman konum tespiti değil de 
         // en yakın adaların cart curtu tutulup onun güncellenip ona göre bi performans
         // içinde index , pozisyon ve rotasyon bulunan bir struct
         // NetworkList<> içinde bu struct olacak şekilde
         // yapılabilir.
-        foreach (WorldIsland island in temp)
-        {
-            if (CheckIslandToUnLoad(island))
-                unLoadQueue.Add(island);
-        }
         foreach (WorldIsland island in worldIslands)
         {
-            if (CheckIslandToLoad(island))
+            if (shipCoordinate.CalculateDistanceBetweenTwoPoints(shipCoordinate.latitude.Value, shipCoordinate.longitude.Value, island.latitude, island.longitude) < island.loadDistance)
             {
-                if (!loadedIslands.Contains(island))
-                    loadQueue.Add(island);
+                if(island.instance!=null)
+                    continue;
+                _ = LoadIsland(island);
+            }   
+            else if(island.instance!=null)
+            {
+                UnloadIsland(island);
             }
         }
-        _ = LoadIslandList(loadQueue);
-        UnloadIslandList(unLoadQueue);
-        loadQueue.Clear();
-        unLoadQueue.Clear();
-
-
     }
 
-    bool CheckIslandToLoad(WorldIsland island)
-    {
-        if (shipCoordinate == null)
-            return false;
-        float dist = shipCoordinate.CalculateDistanceBetweenTwoPoints(shipCoordinate.latitude.Value, shipCoordinate.longitude.Value, island.latitude, island.longitude);
-        //Debug.Log(dist);
-        if (dist <= island.loadDistance)
-            return true;
-        else
-            return false;
-    }
-
-    bool CheckIslandToUnLoad(WorldIsland island)
-    {
-        if (shipCoordinate == null)
-            return false;
-        float dist = shipCoordinate.CalculateDistanceBetweenTwoPoints(shipCoordinate.latitude.Value, shipCoordinate.longitude.Value, island.latitude, island.longitude);
-        //Debug.Log(dist);
-        if (dist >= island.loadDistance || Vector3.Distance(island.instance.transform.position, shipCoordinate.transform.position) >= island.loadDistance)
-            return true;
-        else
-            return false;
-    }
-
-    private async Task LoadIslandList(List<WorldIsland> islands)
-    {
-        var tasks = new List<Task>();
-
-        foreach (WorldIsland island in islands)
-        {
-            tasks.Add(LoadIsland(island));
-        }
-
-        await Task.WhenAll(tasks);
-    }
     private async Task LoadIsland(WorldIsland island)
     {
         float dist = shipCoordinate.CalculateDistanceBetweenTwoPoints(
@@ -145,49 +123,52 @@ public class WorldIslandController : NetworkBehaviour
 
         Vector3 pos = dir * dist + shipCoordinate.transform.position;
         Quaternion rot = Quaternion.identity;
-        SpawnIslandRpc(worldIslands.IndexOf(island), pos, rot);
+        
+        IslandSpawnData data;
+        data.index = worldIslands.IndexOf(island);
+        data.position = pos;
+        data.rotation = rot;
+        data.loadDistance = island.loadDistance;
+        SpawnIslandRpc(data);
     }
     [Rpc(SendTo.Everyone)]
-    void SpawnIslandRpc(int index, Vector3 position, Quaternion rotation)
+    void SpawnIslandRpc(IslandSpawnData data)
     {
-        SpawnStuff(index, position, rotation);
+        _ = SpawnIsland(data);
     }
-    async void SpawnStuff(int index, Vector3 position, Quaternion rotation)
+    async Task SpawnIsland(IslandSpawnData data)
     {
-        WorldIsland island = worldIslands[index];
+        WorldIsland island = worldIslands[data.index];
         if (island.instance != null)
             return;
 
         var op = InstantiateAsync<GameObject>(
             island.prefab,
             null,
-            position,
-            rotation);
+            data.position,
+            data.rotation);
 
-        loadedIslands.Add(island);
+        loadedIslands.Add(data);
         island.instance = (await op)[0];
     }
     [Rpc(SendTo.Everyone)]
     void DestroyIslandRpc(int index)
     {
         Destroy(worldIslands[index].instance);
-        worldIslands[index].instance = null;
-        loadedIslands.Remove(worldIslands.Find(x => x == worldIslands[index]));
-
+     
     }
 
-    void UnloadIslandList(List<WorldIsland> islands)
+    void UnloadIsland(WorldIsland island)
     {
-        foreach (WorldIsland island in islands)
+        int index = worldIslands.IndexOf(island);
+        DestroyIslandRpc(index);
+        foreach(IslandSpawnData data in loadedIslands)
         {
-            int index = worldIslands.IndexOf(island);
-            DestroyIslandRpc(index);
-            /*
-            Destroy(worldIslands.Find(x => x == island).instance);
-            worldIslands.Find(x => x == island).instance = null;
-            loadedIslands.Remove(worldIslands.Find(x => x == island));
-            */
+            if(index == data.index)
+            {
+                loadedIslands.Remove(data);
+                break;
+            }
         }
-
     }
 }
